dd3383cf3311ea25b722887d4897eb57a0096574
William Saidac <sdfsfsds>
2016-11-01 11:26:31 +0100

    working verison with github api

--- index.js
+++ index.js
@@ -3,25 +3,34 @@ const express = require('express'),
       request = require('request'),
       pug = require('pug'),
       morgan = require('morgan');
-      COINMARKETCAP_API_SERVER = 'https://api.coinmarketcap.com/v1/ticker/';
+      GITHUB_API_SERVER = 'https://api.github.com';
 
 var app = express();
 
 const BaseRequest = request.defaults({
   headers: {
-    'Coin-Agent': 'Williams super JS code'
+    'User-Agent': 'Williams super JS code'
   }
-}) ;
+});
 
 app.set('view engine', 'pug');
 
 app.use(morgan('dev'));
 
-app.get('/:id', (req, res) => {
-  BaseRequest.get(COINMARKETCAP_API_SERVER + '/coins/' + req. params.id, (error, response, body) => {
+app.get('/:username', (req, res) => {
+  BaseRequest.get(GITHUB_API_SERVER + '/users/' + req.params.username, (error, response, body) => {
     if (!error) {
-      console.log(body);
-      res.render('coins/show', { coin: JSON.parse(body) });
+      res.render('users/show', { user: JSON.parse(body) });
+    } else {
+      res.status(500).end();
+    }
+  });
+});
+
+app.get('/api/:username', (req, res) => {
+  BaseRequest.get(GITHUB_API_SERVER + '/users/' + req.params.username, (error, response, body) => {
+    if (!error) {
+      res.json(JSON.parse(body));
     } else {
       res.status(500).end();
     }
